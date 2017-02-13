b3ce296a92155e39fbe42cb2c1285a34ecb5838e
grayMou5e <vpilkauskas@gmail.com>
2017-02-13 21:57:27 +0200

    Added project files

---
+++ src/hiptfsbot/Controllers/TfsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace hiptfsbot.Controllers
+{
+    public class TfsController : ApiController
+    {
+        [HttpPost]
+        //[Route("pullRequest/{id}")]
+        public async Task<IHttpActionResult> PullRequest([FromUri]int id)
+        {
+            return Ok(id);
+        } 
+    }
+}
