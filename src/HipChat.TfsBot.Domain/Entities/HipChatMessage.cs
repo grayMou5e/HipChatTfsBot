c73137b17775aed790d91e9263272cffa4c5d59f
grayMou5e <vpilkauskas@gmail.com>
2017-02-17 00:58:33 +0200

    Added checks for HipChatMessage arguments

--- src/HipChat.TfsBot.Domain/Entities/HipChatMessage.cs
+++ src/HipChat.TfsBot.Domain/Entities/HipChatMessage.cs
@@ -1,6 +1,7 @@
 ﻿using HipChat.TfsBot.Domain.ChatOptions;
 using HipChat.TfsBot.Domain.Handlers;
 using HipChat.TfsBot.Domain.TfsMessageBuilders;
+using System;
 using System.Threading.Tasks;
 
 namespace HipChat.TfsBot.Domain.Entities
@@ -17,6 +18,10 @@ namespace HipChat.TfsBot.Domain.Entities
 
         private HipChatMessage(Color color, string message, bool notify, MessageFormat messageFormat)
         {
+            if (!Enum.IsDefined(typeof(Color), color)) { throw new ArgumentException("Incorrect value for enum Color"); }
+            if (!Enum.IsDefined(typeof(MessageFormat), messageFormat)) { throw new ArgumentException("Incorrect value for enum MessageFormat"); }
+            if (string.IsNullOrWhiteSpace(message)) { throw new ArgumentException("Message cannot be null or empty"); }
+
             NotificationColor = color;
             Message = message;
             Notify = notify;
