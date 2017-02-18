aeda09d5e33540537805c0da0bac2021880f4059
grayMou5e <vpilkauskas@gmail.com>
2017-02-18 21:41:31 +0200

    Adding architecture and main pseudo code for room controller

---
+++ HipChat.TfsBot.DataAccess/Clients/RoomClient.cs
@@ -0,0 +1,37 @@
+﻿using HipChat.TfsBot.Domain.Entities;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace HipChat.TfsBot.DataAccess.Clients
+{
+    public class RoomClient
+    {
+        private string _connectionString;
+
+        public RoomClient(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString)) { throw new ArgumentException(); }
+
+            _connectionString = connectionString;
+        }
+
+        public async Task<bool> InsertAsync(Room room)
+        {
+            using (SqlConnection openCon = new SqlConnection(_connectionString))
+            {
+                var query = "";
+
+                using(SqlCommand com = new SqlCommand(query))
+                {
+                    com.Connection = openCon;
+                    openCon.Open();
+
+                    await com.ExecuteNonQueryAsync();
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/HipChat.TfsBot.Domain/Entities/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HipChat.TfsBot.Domain.Entities
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public int RoomId { get; set; }
+        public string AuthToken { get; set; }
+        public string Secret { get; set; }
+    }
+}
