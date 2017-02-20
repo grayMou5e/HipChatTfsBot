f7e5077859295a8dba825fc5a499fee073a6e4e4
grayMou5e <vpilkauskas@gmail.com>
2017-02-21 00:32:27 +0200

    Added CreateDatabase sql script

---
+++ src/HipChat.TfsBot.DataAccess/SqlScripts/CreateDatabase.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE HipChat_TfsBot;
+GO
+
+USE [HipChat_TfsBot];
+GO
+
+CREATE TABLE [Room]
+(
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
+	[RoomId] [integer] NOT NULL UNIQUE,
+	[AuthToken] [nvarchar] (128) NOT NULL,
+	[Secret] [nvarchar] (256) NOT NULL
+);
+GO
